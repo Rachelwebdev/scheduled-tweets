72d905ac08340a8aecd35ac31c4f20e493a06184
Rachel Isaac <rachelisaac995@gmail.com>
2023-05-12 18:07:25 +0400

    Add flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,6 @@
 class MainController < ApplicationController
-  def index; end
+  def index
+    flash.now[:notice] = "Logged in successfully"
+    flash.now[:alert] = "Invalid email or password"
+ end
 end
